c76a74f3178340017c75878800266858efa37fa8
king824 <i@jxb.name>
2015-04-03 09:21:22 +0800

    first commit

---
+++ XBLoadingButton/XBLoadingButton/XBLoadingButton.m
@@ -0,0 +1,21 @@
+//
+//  XBLoadingButton.m
+//  XBLoadingButton
+//
+//  Created by Peter on 15/4/2.
+//  Copyright (c) 2015年 Peter. All rights reserved.
+//
+
+#import "XBLoadingButton.h"
+
+@implementation XBLoadingButton
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
